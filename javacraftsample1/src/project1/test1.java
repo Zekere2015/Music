31c703afe634b5da0c18b9aa5c82231a3d03499d
Zekere2015 <108777170+Zekere2015@users.noreply.github.com>
2022-07-12 12:09:34 -0400

    commit from eclipse

---
+++ javacraftsample1/src/project1/test1.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Greetings;from craft common15");
+      System.out.println("thank you");
+	}
+
+}
