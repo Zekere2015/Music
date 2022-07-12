ac48939c0515d69d397bd4111e07fa10ca1faef8
Zekere2015 <108777170+Zekere2015@users.noreply.github.com>
2022-07-12 13:34:44 -0400

    Sample project created.

---
+++ sampelproject2/src/sampelprogect2/Class2.java
@@ -0,0 +1,13 @@
+package sampelprogect2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("we tray to practice");
+	System.out.println("thank you");
+	System.out.println("we love you");
+
+	}
+
+}
